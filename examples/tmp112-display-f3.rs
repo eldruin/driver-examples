cdb922dbe4c00b69b760f061074bc738bedfb9c6
Diego Barrios Romero <eldruin@gmail.com>
2019-03-16 19:11:00 +0100

    Add example for tmp112 temperature sensor

---
+++ examples/tmp112-display-f3.rs
@@ -0,0 +1,87 @@
+//! Continuously read the temperature with the TMP112 sensor and display it in
+//! an SSD1306 OLED display.
+//!
+//! This example is runs on the STM32F3 Discovery board using I2C1.
+//!
+//! ```
+//! F3  <-> TMP112 <-> Display
+//! GND <-> GND    <-> GND
+//! VCC <-> +5V    <-> +5V
+//! PB7 <-> SDA    <-> SDA
+//! PB6 <-> SCL    <-> SCL
+//! ```
+//! Run with:
+//! `cargo run --example tmp112-display-f3 --target thumbv7em-none-eabihf`
+
+#![deny(unsafe_code)]
+#![no_std]
+#![no_main]
+
+// panic handler
+extern crate embedded_graphics;
+extern crate panic_semihosting;
+
+use cortex_m_rt::entry;
+use embedded_graphics::fonts::Font6x8;
+use embedded_graphics::prelude::*;
+use f3::{
+    hal::{delay::Delay, i2c::I2c, prelude::*, stm32f30x},
+    led::Led,
+};
+use ssd1306::prelude::*;
+use ssd1306::Builder;
+use tmp1x2::{SlaveAddr, Tmp1x2};
+
+use core::fmt::Write;
+#[entry]
+fn main() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = stm32f30x::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut led: Led = gpioe
+        .pe9
+        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
+        .into();
+    let mut delay = Delay::new(cp.SYST, clocks);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
+    let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+    let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+
+    let i2c = I2c::i2c1(dp.I2C1, (scl, sda), 100.khz(), clocks, &mut rcc.apb1);
+
+    let manager = shared_bus::BusManager::<cortex_m::interrupt::Mutex<_>, _>::new(i2c);
+    let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(manager.acquire()).into();
+
+    disp.init().unwrap();
+    disp.flush().unwrap();
+
+    let mut tmp112 = Tmp1x2::new(manager.acquire(), SlaveAddr::default());
+
+    let mut buffer: heapless::String<heapless::consts::U64> = heapless::String::new();
+    loop {
+        // Blink LED 0 to check that everything is actually running.
+        // If the LED 0 is off, something went wrong.
+        led.on();
+        delay.delay_ms(50_u16);
+        led.off();
+        delay.delay_ms(50_u16);
+
+        // If there was an error, it will print 500.00ºC.
+        let temp_c = tmp112.read_temperature().unwrap_or(500.0);
+
+        buffer.clear();
+        write!(buffer, "Temperature: {:.2}ºC", temp_c).unwrap();
+        disp.draw(
+            Font6x8::render_str(&buffer)
+                .with_stroke(Some(1u8.into()))
+                .into_iter(),
+        );
+        disp.flush().unwrap();
+    }
+}
