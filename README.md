95c776c1faace4aa73cc208738a0934e60960dce
Diego Barrios Romero <eldruin@gmail.com>
2019-03-31 18:06:19 +0200

    Add example for veml6040

--- README.md
+++ README.md
@@ -17,6 +17,7 @@ This repository includes examples of using devices through these drivers:
 |[Pwm-pca9685]  | 16-pin PWM port expander / LED driver.                    | I2C       |                                   |
 |[Tmp006]       | Non-contact infrared (IR) thermopile temperature sensor   | I2C       | [Intro blog post][blog-tmp006]    |
 |[Tmp1x2]       | Temperature sensors.                                      | I2C       | [Intro blog post][blog-tmp1x2]    |
+|[Veml6040]     | RGBW light color sensor.                                  | I2C       |                                   |
 
 These examples use the STM32F3Discovery board. At the beginning of each example the setup
 and behavior is described. Many of them also use an SSD1306 OLED display.
@@ -63,6 +64,7 @@ be dual licensed as above, without any additional terms or conditions.
 [Pwm-pca9685]: https://crates.io/crates/pwm-pca9685
 [Tmp006]: https://crates.io/crates/tmp006
 [Tmp1x2]: https://crates.io/crates/tmp1x2
+[Veml6040]: https://crates.io/crates/veml6040
 
 [blog-ads1x1x]: https://blog.eldruin.com/ads1x1x-analog-to-digital-converter-driver-in-rust/
 [blog-eeprom24x]: https://blog.eldruin.com/24x-serial-eeprom-driver-in-rust/
---
+++ examples/veml6040-color-display-f3.rs
@@ -0,0 +1,97 @@
+//! Continuously read the color / ambient light sensor data and print it to
+//! an SSD1306 OLED display.
+//!
+//! This example is runs on the STM32F3 Discovery board using I2C1.
+//!
+//! ```
+//! F3   <-> VEML6040 <-> Display
+//! GND  <-> GND      <-> GND
+//! 3.3V <-> VCC      <-> VDD
+//! PB7  <-> SDA      <-> SDA
+//! PB6  <-> SCL      <-> SCL
+//! ```
+//!
+//! Beware that the VEML6040 runs on 3.3V but PB6 and PB7 run on 5V level
+//! so make sure to put a logic level shifter in between.
+//!
+//! Run with:
+//! `cargo run --example veml6040-color-display-f3 --target thumbv7em-none-eabihf`,
+
+#![deny(unsafe_code)]
+#![no_std]
+#![no_main]
+
+// panic handler
+extern crate embedded_graphics;
+extern crate panic_semihosting;
+
+use cortex_m_rt::entry;
+use embedded_graphics::fonts::Font6x8;
+use embedded_graphics::prelude::*;
+use f3::{
+    hal::{delay::Delay, i2c::I2c, prelude::*, stm32f30x},
+    led::Led,
+};
+use ssd1306::prelude::*;
+use ssd1306::Builder;
+use veml6040::Veml6040;
+
+use core::fmt::Write;
+#[entry]
+fn main() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = stm32f30x::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut led: Led = gpioe
+        .pe9
+        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
+        .into();
+    let mut delay = Delay::new(cp.SYST, clocks);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
+    let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+    let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+
+    let i2c = I2c::i2c1(dp.I2C1, (scl, sda), 400.khz(), clocks, &mut rcc.apb1);
+
+    let manager = shared_bus::BusManager::<cortex_m::interrupt::Mutex<_>, _>::new(i2c);
+    let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(manager.acquire()).into();
+
+    disp.init().unwrap();
+    disp.flush().unwrap();
+
+    let mut sensor = Veml6040::new(manager.acquire());
+    sensor.enable().unwrap();
+
+    loop {
+        // Blink LED 0 to check that everything is actually running.
+        // If the LED 0 is off, something went wrong.
+        led.on();
+        delay.delay_ms(50_u16);
+        led.off();
+        delay.delay_ms(50_u16);
+
+        let mut buffer: heapless::String<heapless::consts::U64> = heapless::String::new();
+
+        let light = sensor.read_all_channels().unwrap();
+
+        write!(
+            buffer,
+            "R {} G {} B {} W {}     ",
+            light.red, light.green, light.blue, light.white
+        )
+        .unwrap();
+
+        disp.draw(
+            Font6x8::render_str(&buffer)
+                .with_stroke(Some(1u8.into()))
+                .into_iter(),
+        );
+        disp.flush().unwrap();
+    }
+}
