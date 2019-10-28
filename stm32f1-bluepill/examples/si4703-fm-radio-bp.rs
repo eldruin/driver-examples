2ed02406d909207371b5e28d2fa0c5d03be625c0
Diego Barrios Romero <eldruin@gmail.com>
2019-10-28 19:28:39 +0100

    Use seeking with buttons

--- stm32f1-bluepill/examples/si4703-fm-radio-bp.rs
+++ stm32f1-bluepill/examples/si4703-fm-radio-bp.rs
@@ -1,16 +1,18 @@
-//! Automatically seek for an FM radio channel every 5 seconds using
-//! an Si4703 FM radio receiver (turner)
+//! Seek an FM radio channel when pressing two buttons "Seek down" / "Seek up"
+//! using an Si4703 FM radio receiver (turner)
 //!
 //! This example is runs on the STM32F103 "Bluepill" board using I2C1.
 //!
 //! ```
-//! BP   <-> Si4703
-//! GND  <-> GND
-//! +5V  <-> VCC
-//! PB8  <-> SCLK
-//! PB9  <-> SDIO
-//! PB7  <-> RST
-//! PB6  <-> GPIO2
+//! BP    <-> Si4703
+//! GND   <-> GND
+//! +3.3V <-> VCC
+//! PB8   <-> SCLK
+//! PB9   <-> SDIO
+//! PB7   <-> RST
+//! PB6   <-> GPIO2
+//! PB10             <-> Seek up button   <-> +3.3V
+//! PB11             <-> Seek down button <-> +3.3V
 //! ```
 //!
 //! Run with:
@@ -24,14 +26,15 @@ use cortex_m_rt::entry;
 use embedded_hal::digital::v2::{InputPin, OutputPin};
 use nb::block;
 use panic_semihosting as _;
+use si4703::{
+    reset as reset_si4703, ChannelSpacing, DeEmphasis, SeekDirection, SeekMode, Si4703, Volume,
+};
 use stm32f1xx_hal::{
     delay::Delay,
     i2c::{BlockingI2c, DutyCycle, Mode},
     pac,
     prelude::*,
 };
-use si470x::{reset as reset_si470x, Si470x, SeekMode,SeekDirection, DeEmphasis};
-
 
 #[entry]
 fn main() -> ! {
@@ -42,20 +45,20 @@ fn main() -> ! {
     let mut rcc = dp.RCC.constrain();
 
     let clocks = rcc.cfgr.freeze(&mut flash.acr);
-
     let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
-
     let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
 
     let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
     let mut sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
     let mut rst = gpiob.pb7.into_push_pull_output(&mut gpiob.crl);
-    let mut stcint = gpiob.pb6;
+    let stcint = gpiob.pb6.into_pull_up_input(&mut gpiob.crl);
+    let seekdown = gpiob.pb11.into_pull_down_input(&mut gpiob.crh);
+    let seekup = gpiob.pb10.into_pull_down_input(&mut gpiob.crh);
     let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
     let mut delay = Delay::new(cp.SYST, clocks);
 
-    reset_si470x(&mut rst, &mut sda, &mut delay).unwrap();
+    reset_si4703(&mut rst, &mut sda, &mut delay).unwrap();
     let i2c = BlockingI2c::i2c1(
         dp.I2C1,
         (scl, sda),
@@ -71,16 +74,17 @@ fn main() -> ! {
         1000,
         1000,
     );
+    let manager = shared_bus::BusManager::<cortex_m::interrupt::Mutex<_>, _>::new(i2c);
 
-    let mut radio = Si470x::new_si4703(i2c);
+    let mut radio = Si4703::new(manager.acquire());
     radio.enable_oscillator().unwrap();
     delay.delay_ms(500_u16);
     radio.enable().unwrap();
     delay.delay_ms(110_u16);
 
-    radio.set_volume(1).unwrap();
+    radio.set_volume(Volume::Dbfsm28).unwrap();
     radio.set_deemphasis(DeEmphasis::Us50).unwrap();
-    radio.configure_seek(SeekMode::Wrap, SeekDirection::Up).unwrap();
+    radio.set_channel_spacing(ChannelSpacing::Khz100).unwrap();
     radio.unmute().unwrap();
     loop {
         // Blink LED 0 every time a new seek is started
@@ -88,8 +92,17 @@ fn main() -> ! {
         led.set_low().unwrap();
         delay.delay_ms(50_u16);
         led.set_high().unwrap();
+        delay.delay_ms(50_u16);
+        let should_seek_down = seekdown.is_high().unwrap();
+        let should_seek_up = seekup.is_high().unwrap();
+        if should_seek_down || should_seek_up {
+            let direction = if should_seek_down {
+                SeekDirection::Down
+            } else {
+                SeekDirection::Up
+            };
 
-        block!(radio.seek_with_stc_int_pin(&mut stcint)).unwrap();
-        delay.delay_ms(5000_u16);
-   }
+            block!(radio.seek_with_stc_int_pin(SeekMode::Wrap, direction, &stcint)).unwrap();
+        }
+    }
 }
