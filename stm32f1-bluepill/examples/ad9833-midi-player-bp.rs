4012b6c489e68b45c6aecd39b5acf02b7ca47561
Diego Barrios Romero <eldruin@gmail.com>
2019-11-10 20:24:50 +0100

    Update AD9833 example and port to bluepill

--- stm32f1-bluepill/examples/ad9833-midi-player-bp.rs
+++ stm32f1-bluepill/examples/ad9833-midi-player-bp.rs
@@ -4,84 +4,75 @@
 //! You can see a video of this running here:
 //! https://blog.eldruin.com/ad983x-waveform-generator-dds-driver-in-rust/
 //!
-//! This example is runs on the STM32F3 Discovery board using SPI1.
+//! This example is runs on the STM32F103 "Bluepill" board using  SPI1.
 //!
 //! ```
 //! F3   <-> AD9833  <-> Amplifier
 //! GND  <-> VSS     <-> GND
-//! 3.3V <-> VDD
-//! 5V               <-> VCC
+//! 3.3V <-> VDD     <-> VCC
+//! PA4  <-> FSYNC
 //! PA5  <-> CLK
 //! PA7  <-> DAT
-//! PB5  <-> FSYNC
 //!          OUT     <-> IN
 //! ```
 //!
 //! You will need an amplifier like the PAM8403 or similar and a speaker.
 //!
 //! Run with:
-//! `cargo run --example ad9833-midi-player-f3 --target thumbv7em-none-eabihf`,
+//! `cargo run --example ad9833-midi-player-bp`,
 
 #![deny(unsafe_code)]
 #![no_std]
 #![no_main]
 
-// panic handler
-extern crate panic_semihosting;
-
+use panic_semihosting as _;
 use ad983x::{Ad983x, FrequencyRegister, MODE};
 use cortex_m_rt::entry;
-use embedded_hal::blocking::delay::DelayMs;
 use embedded_hal::digital::v2::OutputPin;
-use f3::{
-    hal::{
-        delay::Delay, flash::FlashExt, gpio::GpioExt, rcc::RccExt, spi::Spi, stm32f30x,
-        time::U32Ext,
-    },
-    led::Led,
+use stm32f1xx_hal::{
+    delay::Delay,
+    spi::Spi,
+    pac,
+    prelude::*
 };
 use libm;
 
 #[entry]
 fn main() -> ! {
     let cp = cortex_m::Peripherals::take().unwrap();
-    let dp = stm32f30x::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
 
     let mut flash = dp.FLASH.constrain();
     let mut rcc = dp.RCC.constrain();
-    let mut gpioe = dp.GPIOE.split(&mut rcc.ahb);
-    let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
-    let mut led: Led = gpioe
-        .pe9
-        .into_push_pull_output(&mut gpioe.moder, &mut gpioe.otyper)
-        .into();
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
     let mut delay = Delay::new(cp.SYST, clocks);
 
-    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
-    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
-
-    // SPI configuration
-    let sck = gpioa.pa5.into_af5(&mut gpioa.moder, &mut gpioa.afrl);
-    let miso = gpioa.pa6.into_af5(&mut gpioa.moder, &mut gpioa.afrl);
-    let mosi = gpioa.pa7.into_af5(&mut gpioa.moder, &mut gpioa.afrl);
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+    let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+    
+    // SPI1
+    let sck = gpioa.pa5.into_alternate_push_pull(&mut gpioa.crl);
+    let miso = gpioa.pa6;
+    let mosi = gpioa.pa7.into_alternate_push_pull(&mut gpioa.crl);
+    let mut cs = gpioa.pa4.into_push_pull_output(&mut gpioa.crl);
 
     let spi = Spi::spi1(
         dp.SPI1,
         (sck, miso, mosi),
+        &mut afio.mapr,
         MODE,
-        1.mhz(),
+        1_u32.mhz(),
         clocks,
         &mut rcc.apb2,
     );
 
-    let mut chip_select = gpiob
-        .pb5
-        .into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
 
-    chip_select.set_high().unwrap();
+    cs.set_high().unwrap();
 
-    let mut synth = Ad983x::new_ad9833(spi, chip_select);
+    let mut synth = Ad983x::new_ad9833(spi, cs);
     synth.reset().unwrap();
     synth.enable().unwrap();
 
@@ -90,10 +81,10 @@ fn main() -> ! {
     loop {
         // Blink LED 0 to check that everything is actually running.
         // If the LED 0 does not blink, something went wrong.
-        led.on();
+        led.set_high().unwrap();
         delay.delay_ms(50_u16);
-        led.off();
-        delay.delay_ms(50_u16);
+        led.set_low().unwrap();
+        delay.delay_ms(25_u16);
 
         let midi_number = table.next().unwrap_or(0);
         let midi_number = f64::from(midi_number);
