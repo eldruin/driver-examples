0ecda61fd73fa7e425effdcf75b853b6d4690e03
Diego Barrios Romero <eldruin@gmail.com>
2019-12-22 12:39:30 +0100

    Add MCP7940N example for BP

---
+++ stm32f1-bluepill/examples/mcp7940n-rtc-display-bp.rs
@@ -0,0 +1,119 @@
+//! Stores the date and time on a MCP7940N real-time clock (RTC).
+//! Then continuously print the date and time.
+//!
+//! Introductory blog post here:
+//! https://blog.eldruin.com/mcp794xx-real-time-clock-rtc-driver-in-rust/
+//!
+//! This example is runs on the STM32F1 "BluePill" board using I2C1.
+//!
+//! ```
+//! BP    <-> MCP7940N <-> Display
+//! GND   <-> GND      <-> GND
+//! +3.3V <-> +3.3V    <-> +3.3V
+//! PB8   <-> SCL      <-> SCL
+//! PB9   <-> SDA      <-> SDA
+//! ```
+//!
+//! Run with:
+//! `cargo run --example mcp7940n-rtc-display-bp`,
+
+#![deny(unsafe_code)]
+#![no_std]
+#![no_main]
+
+use core::fmt::Write;
+use cortex_m_rt::entry;
+use embedded_graphics::{fonts::Font6x8, prelude::*};
+use embedded_hal::digital::v2::OutputPin;
+use mcp794xx::{DateTime, Hours, Mcp794xx, Rtcc};
+use panic_semihosting as _;
+use ssd1306::{prelude::*, Builder};
+use stm32f1xx_hal::{
+    delay::Delay,
+    i2c::{BlockingI2c, DutyCycle, Mode},
+    pac,
+    prelude::*,
+};
+
+#[entry]
+fn main() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+
+    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+    let i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Fast {
+            frequency: 100_000,
+            duty_cycle: DutyCycle::Ratio2to1,
+        },
+        clocks,
+        &mut rcc.apb1,
+        1000,
+        10,
+        1000,
+        1000,
+    );
+
+    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+    let mut delay = Delay::new(cp.SYST, clocks);
+
+    let manager = shared_bus::BusManager::<cortex_m::interrupt::Mutex<_>, _>::new(i2c);
+    let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(manager.acquire()).into();
+
+    disp.init().unwrap();
+    disp.flush().unwrap();
+
+    let mut rtc = Mcp794xx::new_mcp7940n(manager.acquire());
+    let begin = DateTime {
+        year: 2019,
+        month: 1,
+        day: 2,
+        weekday: 3,
+        hour: Hours::H24(4),
+        minute: 5,
+        second: 6,
+    };
+    rtc.set_datetime(&begin).unwrap();
+    rtc.enable().unwrap();
+    loop {
+        // Blink LED 0 to check that everything is actually running.
+        // If the LED 0 is off, something went wrong.
+        led.set_high().unwrap();
+        delay.delay_ms(50_u16);
+        led.set_low().unwrap();
+        delay.delay_ms(50_u16);
+
+        let now = rtc.get_datetime().unwrap();
+
+        let mut buffer: heapless::String<heapless::consts::U32> = heapless::String::new();
+        if let Hours::H24(h) = now.hour {
+            write!(
+                buffer,
+                "{}-{}-{} {} {}:{}:{}   ",
+                now.year, now.month, now.day, now.weekday, h, now.minute, now.second
+            )
+            .unwrap();
+            disp.draw(
+                Font6x8::render_str(&buffer)
+                    .with_stroke(Some(1u8.into()))
+                    .into_iter(),
+            );
+        }
+        disp.flush().unwrap();
+    }
+}
