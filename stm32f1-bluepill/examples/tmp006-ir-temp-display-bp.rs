72fe7d53d85c4be89d586212df764a804eabd738
Diego Barrios Romero <eldruin@gmail.com>
2019-12-22 12:38:37 +0100

    Add TMP006 example for BP

---
+++ stm32f1-bluepill/examples/tmp006-ir-temp-display-bp.rs
@@ -0,0 +1,123 @@
+//! Continuously read the object temperature with the TMP006 and display it in
+//! an SSD1306 OLED display.
+//!
+//! Introductory blog post with some pictures here:
+//! https://blog.eldruin.com/tmp006-contact-less-infrared-ir-thermopile-driver-in-rust/
+//!
+//! This example is runs on the STM32F103 "Bluepill" board using I2C1.
+//!
+//! ```
+//! BP   <-> TMP006 <-> Display
+//! GND  <-> GND    <-> GND
+//! 3.3V <-> VCC    <-> VDD
+//! PB8  <-> SCL    <-> SCL
+//! PB9  <-> SDA    <-> SDA
+//! ```
+//!
+//! Run with:
+//! `cargo run --example temp006-ir-temp-display-bp`,
+
+#![deny(unsafe_code)]
+#![no_std]
+#![no_main]
+
+use core::fmt::Write;
+use cortex_m_rt::entry;
+use embedded_graphics::{fonts::Font6x8, prelude::*};
+use embedded_hal::digital::v2::OutputPin;
+use panic_semihosting as _;
+use ssd1306::{prelude::*, Builder};
+use stm32f1xx_hal::{
+    delay::Delay,
+    i2c::{BlockingI2c, DutyCycle, Mode},
+    pac,
+    prelude::*,
+};
+use nb::block;
+use tmp006::{SlaveAddr, Tmp006};
+
+#[entry]
+fn main() -> ! {
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+
+    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+    let i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Fast {
+            frequency: 400_000,
+            duty_cycle: DutyCycle::Ratio2to1,
+        },
+        clocks,
+        &mut rcc.apb1,
+        1000,
+        10,
+        1000,
+        1000,
+    );
+
+    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+    let mut delay = Delay::new(cp.SYST, clocks);
+
+    let manager = shared_bus::BusManager::<cortex_m::interrupt::Mutex<_>, _>::new(i2c);
+    let mut disp: GraphicsMode<_> = Builder::new().connect_i2c(manager.acquire()).into();
+
+    disp.init().unwrap();
+    disp.flush().unwrap();
+
+    let mut tmp006 = Tmp006::new(manager.acquire(), SlaveAddr::default());
+
+    let mut lines: [heapless::String<heapless::consts::U32>; 2] =
+        [heapless::String::new(), heapless::String::new()];
+    loop {
+        // Blink LED 0 to check that everything is actually running.
+        // If the LED 0 is off, something went wrong.
+        led.set_high().unwrap();
+        delay.delay_ms(50_u16);
+        led.set_low().unwrap();
+        delay.delay_ms(50_u16);
+        lines[0].clear();
+        lines[1].clear();
+
+        let calibration_factor = 6e-14;
+        let temp_k = block!(tmp006.read_object_temperature(calibration_factor)).unwrap();
+        let temp_c = temp_k - 273.15;
+        write!(lines[0], "Temperature: {:.2}ºC", temp_c).unwrap();
+
+        // Read data in raw format
+        let raw_data = block!(tmp006.read_sensor_data()).unwrap();
+        write!(
+            lines[1],
+            "OV: {}, AT: {}",
+            raw_data.object_voltage, raw_data.ambient_temperature
+        )
+        .unwrap();
+
+        disp.draw(
+            Font6x8::render_str(&lines[0])
+                .with_stroke(Some(1u8.into()))
+                .into_iter(),
+        );
+        disp.draw(
+            Font6x8::render_str(&lines[1])
+                .with_stroke(Some(1u8.into()))
+                .translate(Coord::new(0, 12))
+                .into_iter(),
+        );
+        disp.flush().unwrap();
+    }
+}
