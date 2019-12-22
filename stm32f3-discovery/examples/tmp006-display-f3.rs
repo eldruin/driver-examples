744840df2eb7c049f7f89a944de87a44afb0a336
Diego Barrios Romero <eldruin@gmail.com>
2019-12-22 12:37:16 +0100

    Improve example

--- stm32f3-discovery/examples/tmp006-display-f3.rs
+++ stm32f3-discovery/examples/tmp006-display-f3.rs
@@ -20,20 +20,15 @@
 #![no_std]
 #![no_main]
 
-// panic handler
-extern crate embedded_graphics;
-extern crate panic_semihosting;
-
 use cortex_m_rt::entry;
-use embedded_graphics::fonts::Font6x8;
-use embedded_graphics::prelude::*;
+use embedded_graphics::{fonts::Font6x8, prelude::*};
 use f3::{
     hal::{delay::Delay, i2c::I2c, prelude::*, stm32f30x},
     led::Led,
 };
 use nb::block;
-use ssd1306::prelude::*;
-use ssd1306::Builder;
+use panic_semihosting as _;
+use ssd1306::{prelude::*, Builder};
 use tmp006::{SlaveAddr, Tmp006};
 
 use core::fmt::Write;
@@ -67,7 +62,8 @@ fn main() -> ! {
 
     let mut tmp006 = Tmp006::new(manager.acquire(), SlaveAddr::default());
 
-    let mut buffer: heapless::String<heapless::consts::U64> = heapless::String::new();
+    let mut lines: [heapless::String<heapless::consts::U32>; 2] =
+        [heapless::String::new(), heapless::String::new()];
     loop {
         // Blink LED 0 to check that everything is actually running.
         // If the LED 0 is off, something went wrong.
@@ -75,25 +71,32 @@ fn main() -> ! {
         delay.delay_ms(50_u16);
         led.off();
         delay.delay_ms(50_u16);
-        buffer.clear();
+        lines[0].clear();
+        lines[1].clear();
+
         let calibration_factor = 6e-14;
         let temp_k = block!(tmp006.read_object_temperature(calibration_factor)).unwrap();
         let temp_c = temp_k - 273.15;
-        write!(buffer, "Temperature: {:.2}ºC", temp_c).unwrap();
-        /* Uncomment this and comment out the previous write call to see the raw sensor
-        data as well.
+        write!(lines[0], "Temperature: {:.2}ºC", temp_c).unwrap();
+
+        // Read data in raw format
         let raw_data = block!(tmp006.read_sensor_data()).unwrap();
         write!(
-            buffer,
-            "OV: {}, AT: {}, T: {:.2}",
-            data.object_voltage,
-            data.ambient_temperature,
-            temp_c
-        ).unwrap();
-        */
+            lines[1],
+            "OV: {}, AT: {}",
+            raw_data.object_voltage, raw_data.ambient_temperature
+        )
+        .unwrap();
+
         disp.draw(
-            Font6x8::render_str(&buffer)
+            Font6x8::render_str(&lines[0])
                 .with_stroke(Some(1u8.into()))
+                .into_iter(),
+        );
+        disp.draw(
+            Font6x8::render_str(&lines[1])
+                .with_stroke(Some(1u8.into()))
+                .translate(Coord::new(0, 12))
                 .into_iter(),
         );
         disp.flush().unwrap();
