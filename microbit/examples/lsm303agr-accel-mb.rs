0f1d385c91ef8ca3674ab3f7015f777d6ac48813
Diego Barrios Romero <eldruin@gmail.com>
2020-09-15 20:07:03 +0200

    Code formatting

--- microbit/examples/lsm303agr-accel-mb.rs
+++ microbit/examples/lsm303agr-accel-mb.rs
@@ -7,17 +7,15 @@
 #![no_main]
 #![no_std]
 
+use core::fmt::Write;
 use cortex_m;
 use cortex_m_rt::entry;
+use lsm303agr::{AccelOutputDataRate, Lsm303agr};
 use microbit::hal::i2c;
 use microbit::hal::prelude::*;
 use microbit::hal::serial;
 use microbit::hal::serial::BAUD115200;
 use panic_halt as _;
-
-use lsm303agr::{AccelOutputDataRate, Lsm303agr};
-
-use core::fmt::Write;
 
 #[entry]
 fn main() -> ! {
