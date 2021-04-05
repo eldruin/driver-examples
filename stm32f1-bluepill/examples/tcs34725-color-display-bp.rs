7b8f7f584b19996c81ee99cac6b52d886749057f
Diego Barrios Romero <eldruin@gmail.com>
2021-04-05 21:02:50 +0200

    Update TCS3472 driver and add example for bluepill

---
+++ stm32f1-bluepill/examples/tcs34725-color-display-bp.rs
@@ -0,0 +1,137 @@
+//! Continuously read the color light sensor data and print it to
+//! an SSD1306 OLED display.
+//!
+//! This example is runs on the STM32F103 "Bluepill" board using I2C1.
+//!
+//! ```
+//! BP   <-> TCS34725 <-> Display
+//! GND  <-> GND      <-> GND
+//! 3.3V <-> VCC      <-> VDD
+//! PB8  <-> SCL      <-> SCL
+//! PB9  <-> SDA      <-> SDA
+//! ```
+//!
+//! Run with:
+//! `cargo embed --example tcs34725-color-display-bp`,
+
+#![deny(unsafe_code)]
+#![no_std]
+#![no_main]
+
+use core::fmt::Write;
+use cortex_m_rt::entry;
+use embedded_graphics::{
+    fonts::{Font6x8, Text},
+    pixelcolor::BinaryColor,
+    prelude::*,
+    style::TextStyleBuilder,
+};
+use embedded_hal::digital::v2::OutputPin;
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+use ssd1306::{prelude::*, Builder, I2CDIBuilder};
+use stm32f1xx_hal::{
+    delay::Delay,
+    i2c::{BlockingI2c, DutyCycle, Mode},
+    pac,
+    prelude::*,
+};
+use tcs3472::{AllChannelMeasurement, Tcs3472};
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("TCS34725 example");
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+
+    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+    let i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Fast {
+            frequency: 400_000.hz(),
+            duty_cycle: DutyCycle::Ratio2to1,
+        },
+        clocks,
+        &mut rcc.apb1,
+        1000,
+        10,
+        1000,
+        1000,
+    );
+
+    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+    let mut delay = Delay::new(cp.SYST, clocks);
+
+    let manager = shared_bus::BusManager::<cortex_m::interrupt::Mutex<_>, _>::new(i2c);
+    let interface = I2CDIBuilder::new().init(manager.acquire());
+    let mut disp: GraphicsMode<_> = Builder::new().connect(interface).into();
+    disp.init().unwrap();
+    disp.flush().unwrap();
+
+    let text_style = TextStyleBuilder::new(Font6x8)
+        .text_color(BinaryColor::On)
+        .build();
+
+    let mut sensor = Tcs3472::new(manager.acquire());
+    sensor.enable().unwrap();
+    sensor.enable_rgbc().unwrap();
+    while !sensor.is_rgbc_status_valid().unwrap() {
+        // wait for measurement to be available
+        delay.delay_ms(50_u8);
+    }
+    let mut lines: [heapless::String<heapless::consts::U32>; 4] = [
+        heapless::String::new(),
+        heapless::String::new(),
+        heapless::String::new(),
+        heapless::String::new(),
+    ];
+    loop {
+        // Blink LED 0 to check that everything is actually running.
+        // If the LED 0 is off, something went wrong.
+        led.set_high().unwrap();
+        delay.delay_ms(50_u16);
+        led.set_low().unwrap();
+        delay.delay_ms(50_u16);
+
+        // If there was an error, it will print 0, 0, 0, 0
+        let measurement = sensor.read_all_channels().unwrap_or(AllChannelMeasurement {
+            red: 0,
+            green: 0,
+            blue: 0,
+            clear: 0,
+        });
+
+        lines[0].clear();
+        lines[1].clear();
+        lines[2].clear();
+        lines[3].clear();
+
+        write!(lines[0], "Red: {}", measurement.red).unwrap();
+        write!(lines[1], "Green: {}", measurement.green).unwrap();
+        write!(lines[2], "Blue: {}", measurement.blue).unwrap();
+        write!(lines[3], "Clear: {}", measurement.clear).unwrap();
+        disp.clear();
+        for (i, line) in lines.iter().enumerate() {
+            Text::new(line, Point::new(0, i as i32 * 16))
+                .into_styled(text_style)
+                .draw(&mut disp)
+                .unwrap();
+        }
+        disp.flush().unwrap();
+    }
+}
