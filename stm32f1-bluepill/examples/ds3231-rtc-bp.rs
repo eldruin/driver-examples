2d82ac6db37d5e7bd49ec7aa2039ef16afbeea82
Diego Barrios Romero <eldruin@gmail.com>
2021-05-22 23:35:14 +0200

    Add DS3231 example for bluepill

---
+++ stm32f1-bluepill/examples/ds3231-rtc-bp.rs
@@ -0,0 +1,85 @@
+//! Stores the date and time on a DS3231 real-time clock (RTC).
+//! Then reads the date and time roughly every second and
+//! prints it through RTT.
+//!
+//! This example is runs on the STM32F1 "BluePill" board using I2C1.
+//!
+//! ```
+//! BP  <-> DS3231
+//! GND <-> GND
+//! +5V <-> +5V
+//! PB8 <-> SCL
+//! PB9 <-> SDA
+//! ```
+//!
+//! Run with:
+//! `cargo embed --example ds3231-rtc-bp`,
+
+#![deny(unsafe_code)]
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use ds323x::{Ds323x, NaiveDate, Rtcc};
+use embedded_hal::digital::v2::OutputPin;
+use panic_rtt_target as _;
+use rtt_target::{rprintln, rtt_init_print};
+use stm32f1xx_hal::{
+    delay::Delay,
+    i2c::{BlockingI2c, DutyCycle, Mode},
+    pac,
+    prelude::*,
+};
+
+#[entry]
+fn main() -> ! {
+    rtt_init_print!();
+    rprintln!("DS3231 example");
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let dp = pac::Peripherals::take().unwrap();
+
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let mut afio = dp.AFIO.constrain(&mut rcc.apb2);
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+
+    let scl = gpiob.pb8.into_alternate_open_drain(&mut gpiob.crh);
+    let sda = gpiob.pb9.into_alternate_open_drain(&mut gpiob.crh);
+
+    let i2c = BlockingI2c::i2c1(
+        dp.I2C1,
+        (scl, sda),
+        &mut afio.mapr,
+        Mode::Fast {
+            frequency: 100_000.hz(),
+            duty_cycle: DutyCycle::Ratio2to1,
+        },
+        clocks,
+        &mut rcc.apb1,
+        1000,
+        10,
+        1000,
+        1000,
+    );
+
+    let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
+    let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+    let mut delay = Delay::new(cp.SYST, clocks);
+
+    let mut rtc = Ds323x::new_ds3231(i2c);
+    let begin = NaiveDate::from_ymd(2020, 5, 2).and_hms(10, 21, 34);
+    rtc.set_datetime(&begin).unwrap();
+    loop {
+        led.set_high().unwrap();
+        delay.delay_ms(250_u16);
+        led.set_low().unwrap();
+        delay.delay_ms(750_u16);
+
+        let now = rtc.get_datetime().unwrap();
+        rprintln!("Date/Time: {:?}", now);
+    }
+}
